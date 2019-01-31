e492ea3d06f848d36dc6f830eeb523c4b29d99a4
Maxim Vasiliew <you@example.com>
2019-01-31 22:08:56 +0300

    КОМИТ

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\vladi\PycharmProjects\metadataParser\venv"
+$env:VIRTUAL_ENV="C:\Users\Максим\PycharmProjects\metadataParser\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\vladi\PycharmProjects\metadataParser\venv"
+set "VIRTUAL_ENV=C:\Users\Максим\PycharmProjects\metadataParser\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
